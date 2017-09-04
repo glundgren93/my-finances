4761c77b0d9c193860ab5fa639d73d84047f57c4
Gabriel Lundgren <gabriel.lundgren@eldorado.org.br>
2017-09-04 17:05:01 -0300

    changing ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import uuidv1 from "uuid/v1";
 import "./App.css";
 
-/////////////////////////////////////
+/**
+ * Renders input based on received props
+ */
 class Input extends Component {
   constructor(props) {
     super(props);
@@ -17,8 +20,23 @@ class Input extends Component {
     return <input type={this.props.type} onChange={this.onChange} />;
   }
 }
-/////////////////////////////////////
-class Cell extends Component {
+
+/**
+ * Cell containing strong with text received from props
+ * @type {Object}
+ */
+const StrongCell = ({ text, className, style }) => {
+  return (
+    <div className={className} style={style}>
+      <strong>{text}</strong>
+    </div>
+  );
+};
+
+/**
+ * Cell containing input component
+ */
+class InputCell extends Component {
   constructor(props) {
     super(props);
 
@@ -51,6 +69,9 @@ class Cell extends Component {
   }
 }
 
+/**
+ * Collection of Cells
+ */
 class Row extends Component {
   constructor(props) {
     super(props);
@@ -65,6 +86,10 @@ class Row extends Component {
     this.onValueChange = this.onValueChange.bind(this);
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onDateChange = this.onDateChange.bind(this);
+    this.deleteRow = this.deleteRow.bind(this);
+  }
+  componentWillMount() {
+    this.setState({ id: this.props.id });
   }
 
   // used on value cell
@@ -76,41 +101,79 @@ class Row extends Component {
 
   // used on title cell
   onTitleChange(title) {
-    this.setState({ title });
+    this.setState({ title }, () => {
+      this.props.saveRow(this.state);
+    });
   }
 
   // used on date cell
   onDateChange(date) {
-    this.setState({ date });
+    this.setState({ date }, () => {
+      this.props.saveRow(this.state);
+    });
+  }
+
+  deleteRow(row) {
+    this.props.deleteRow(row);
   }
 
   render() {
     return (
-      <div className="row">
-        <Cell type="date" onChange={this.onDateChange} />
-        <Cell type="text" onChange={this.onTitleChange} />
-        <Cell type="number" onChange={this.onValueChange} />
+      <div className="row datas">
+        <button onClick={() => this.deleteRow(this.state.id)}>x</button>
+        <StrongCell text={this.state.id} className="cell header" />
+        <InputCell type="text" onChange={this.onDateChange} />
+        <InputCell type="text" onChange={this.onTitleChange} />
+        <InputCell type="number" onChange={this.onValueChange} />
       </div>
     );
   }
 }
 
+/**
+ * Collection of Rows
+ */
 class Grid extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      rows: [],
+      rows: [{ id: uuidv1(), date: "", title: "", value: 0 }],
       rowsCount: 1 // number of rows
     };
 
     this.saveRow = this.saveRow.bind(this);
+    this.deleteRow = this.deleteRow.bind(this);
     this.addRow = this.addRow.bind(this);
+    this.createRowObj = this.createRowObj.bind(this);
   }
 
-// adds new rows
+  createRowObj() {
+    let { rows } = this.state;
+    let len = rows.length;
+
+    return {
+      id: uuidv1(),
+      date: "",
+      title: "",
+      value: 0
+    };
+  }
+
+  // adds new rows
   addRow() {
-    this.setState({ rowsCount: this.state.rowsCount + 1 });
+    let { rows } = this.state;
+    let len = rows.length;
+
+    // this.setState({ rowsCount: this.state.rowsCount + 1 });
+    this.setState({ rows: [...rows, this.createRowObj()] });
+  }
+
+  deleteRow(id) {
+    let { rows } = this.state;
+    this.setState({ rows: [...rows.filter(x => x.id !== id)] }, () => {
+      this.forceUpdate();
+    });
   }
 
   /**
@@ -130,33 +193,54 @@ class Grid extends Component {
   }
 
   render() {
+    let { headerStyle } = this.props;
     let values = this.state.rows.map(x => x.value);
-    let rows = [];
-
-    for (let i = 0; i < this.state.rowsCount; i++) {
-      rows = [...rows, <Row id={i} saveRow={this.saveRow} />];
-    }
+    let rows = this.state.rows.map((current, index) => (
+      <Row id={current.id} saveRow={this.saveRow} deleteRow={this.deleteRow} />
+    ));
 
     return (
-      <div className="main">
-        <div>
-          {rows}
+      <div className="grid">
+        <div className="main">
+          <div className="row title">
+            <section>
+              <h1>{this.props.title}</h1>
+            </section>
+            <section>
+              <button onClick={this.addRow}>Add row</button>
+            </section>
+          </div>
+          <div className="row headers">
+            <StrongCell text="ID" className={headerStyle} />
+            <StrongCell text="Data" className={headerStyle} />
+            <StrongCell text="Categoria" className={headerStyle} />
+            <StrongCell text="Valor" className={headerStyle} />
+          </div>
+          <div>{rows}</div>
+          <div className="row result">
+            <div className="cell">
+              <h3>Total: R$ {values.length > 0 ? values.reduce((x, y) => x + y) : 0}</h3>
+            </div>
+          </div>
         </div>
-        <h2>
-          Total: R$ {values.length > 0 ? values.reduce((x, y) => x + y) : 0}
-        </h2>
-        <button onClick={this.addRow}>Add row</button>
       </div>
     );
   }
 }
 
+/**
+ * Collection of Grids
+ */
 class App extends Component {
   render() {
     return (
       <div>
-        <Grid />
-        <Grid />
+        <section>
+          <Grid title="Receitas" headerStyle="cell positive" />
+        </section>
+        <section>
+          <Grid title="Despesas" headerStyle="cell negative" />
+        </section>
       </div>
     );
   }
